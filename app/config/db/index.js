1fc9ac5cbf2c04c578888b850f5e080c3c7efdb1
Gabriel Cesario <gcesario203@gmail.com>
2022-12-30 11:54:00 -0300

    fix: esqueci alguns pacotes

--- app/config/db/index.js
+++ app/config/db/index.js
@@ -1,5 +1,6 @@
 export default {
     HOST: process.env.DB_HOST || 'localhost:3306',
     USER: process.env.DB_USER || 'root',
-    PASSWORD: process.env.DB_PASSWORD || 'root'
+    PASSWORD: process.env.DB_PASSWORD || 'root',
+    DATABASE_NAME: process.env.DB_NAME || 'desafio02'
 } 
